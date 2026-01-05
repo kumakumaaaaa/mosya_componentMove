4380315e433b6b6ece649425ee31d1ebd4debe96
ToshiyaKumagai <toshiyakumagai@ToshiyaKumagainoMacBook-Air-2.local>
2026-01-05 18:07:56 +0900

    【３章】Reactで入力イベントを扱う|初期値

---
+++ script.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
+
+export default function App() {
+  const [answer, setAnswer] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [answered, setAnswered] = useState(false);
+  return (
+    <div className="quiz-wrap">
+      <h1 className="quiz-title">クイズ</h1>
+      <p className="quiz-question">Q.日本で一番高い山は？</p>
+      <input placeholder="回答を入力" type="text" value={answer} />
+      <button>回答する</button>
+      {/* 👇正解の場合と不正解の場合、それぞれに対して下にメッセージを表示するようにしてください */}
+    </div>
+  );
+}
+
+export const root = createRoot(document.getElementById("root"));
+root.render(<App />);
