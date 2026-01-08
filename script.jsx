4fbdc33faaa52d2d592b38acda9203bae4b644af
ToshiyaKumagai <toshiyakumagai@ToshiyaKumagainoMacBook-Air-2.local>
2026-01-08 21:50:03 +0900

    【３章】カスタムコンポーネントでイベントを扱う|合格

--- script.jsx
+++ script.jsx
@@ -1,33 +1,32 @@
 import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 
+import { ColorButton } from "./color-button";
+
+const colors = ["#EF4444", "#F59E0B", "#10B981", "#3B82F6", "#6366F1"];
+
 export default function App() {
-  const [answer, setAnswer] = useState("");
-  const [success, setSuccess] = useState(false);
-  const [answered, setAnswered] = useState(false);
+  const [color, setColor] = useState(colors[0]);
+
+  const handleSelectColor = (color) => {
+    // ここに選択された色をstateに反映するコードを書いてください
+    setColor(color);
+  };
+
   return (
-    <div className="quiz-wrap">
-      <h1 className="quiz-title">クイズ</h1>
-      <p className="quiz-question">Q.日本で一番高い山は？</p>
-      <input
-        placeholder="回答を入力"
-        type="text"
-        value={answer}
-        onChange={(e) => {
-          setAnswer(e.target.value);
-          setAnswered(false);
-        }}
-      />
-      <button
-        onClick={() => {
-          setSuccess(answer === "富士山");
-          setAnswered(true);
-        }}
-      >
-        回答する
-      </button>
-      {/* 👇正解の場合と不正解の場合、それぞれに対して下にメッセージを表示するようにしてください */}
-      {answered && <p className="result">{success ? "正解！" : "不正解！"}</p>}
+    <div className="mt-10 flex flex-col items-center space-y-4 p-6 bg-white dark:bg-zinc-900 rounded-xl shadow-sm border border-zinc-200 dark:border-zinc-800 max-w-md mx-auto">
+      <h2 className="text-xl font-semibold">Color Picker</h2>
+      <div className="w-16 h-16 border border-zinc-200 dark:border-zinc-800 rounded-lg overflow-hidden">
+        <div
+          className="preview w-full h-full"
+          style={{ backgroundColor: color }}
+        />
+      </div>
+      <div className="flex space-x-2 mt-4">
+        {colors.map((color) => (
+          <ColorButton color={color} key={color} onSelect={handleSelectColor} />
+        ))}
+      </div>
     </div>
   );
 }
